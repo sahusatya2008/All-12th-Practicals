eb4bbf07338dc18f388bbf8df6840e7a5d91c81c
sahusatya2008 <71000505+sahusatya2008@users.noreply.github.com>
2025-12-28 18:47:44 +0530

    Update Practical 2.py

--- Practical 2.py
+++ Practical 2.py
@@ -1,6 +1,5 @@
-#2 Read a text file line by line and display each word separated by a # (Pound).
 # Open the file in read mode.
-with open('four file.txt', 'r') as file:
+with open('input.txt', 'r') as file:
     # Read file line by line
     for line in file:
         # Remove multiple and extra spaces
