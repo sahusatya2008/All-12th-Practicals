893e835a7306e02200970d79ac7a3e0e856ab8fb
sahusatya2008 <71000505+sahusatya2008@users.noreply.github.com>
2025-12-28 16:19:29 +0530

    Update practical4.py

--- practical4.py
+++ practical4.py
@@ -1,4 +1,4 @@
-#Remove all the line that contain the character "a" in the file and write to another file.
+#4. Remove all the line that contain the character "a" in the file and write to another file.
 with open('input.txt','r') as src, open('output.txt','w')as dest:
     for line in src:
         if 'a' not in line and 'A' not in line:
